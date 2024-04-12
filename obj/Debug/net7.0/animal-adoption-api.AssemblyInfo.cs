3c77b89a92979fabbc168291fe82e63e5d6b8f75
Hamid Baydamirov <baydamirovv35@gmail.com>
2024-04-13 02:43:26 +0400

    deletedcommit

--- obj/Debug/net7.0/animal-adoption-api.AssemblyInfo.cs
+++ obj/Debug/net7.0/animal-adoption-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("animal-adoption-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad31d4bb8f4a63a3c01887fff3088900e1f611e")]
 [assembly: System.Reflection.AssemblyProductAttribute("animal-adoption-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("animal-adoption-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
