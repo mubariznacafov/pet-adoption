5f14ee463cb34c0cd8d93f81ca6de11add84ef74
Hamid Baydamirov <baydamirovv35@gmail.com>
2024-04-13 02:21:27 +0400

    deleted guest pet

--- Template/guest-pet/src/components/Veterinary.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Veterinary = () => {
-  return <div>Veterinary</div>;
-};
-
-export default Veterinary;
--- Template/guest-pet/src/components/ZooShop.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ZooShop = () => {
-  return <div>ZooShop</div>;
-};
-
-export default ZooShop;
